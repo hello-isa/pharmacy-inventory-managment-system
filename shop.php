46ca7c67df4652bbc3d9acaf52d99b5f1069b111
Hawks <126882395+h4wks123@users.noreply.github.com>
2023-05-16 22:22:15 +0800

    Update shop.php

--- shop.php
+++ shop.php
@@ -40,8 +40,7 @@ $result = $conn->query($sql);
             background-color: transparent;
             border: transparent;
         }
-        
-        <!-- Style of the modal -->
+
         .modal {
             display: none;
             position: fixed;
@@ -71,9 +70,9 @@ $result = $conn->query($sql);
 
         .close:hover,
         .close:focus {
-        color: black;
-        text-decoration: none;
-        cursor: pointer;
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
         }
     </style>
 
@@ -100,9 +99,9 @@ $result = $conn->query($sql);
             numEl.value = quantity;
             event.preventDefault();
         }
-        
-            // Modal JS
-            document.addEventListener("DOMContentLoaded", function() {
+
+        // Modal JS
+        document.addEventListener("DOMContentLoaded", function() {
             // Get the modal element
             var modal = document.getElementById("myModal");
 
@@ -221,13 +220,14 @@ $result = $conn->query($sql);
     <!-- Modal -->
     <div id="myModal" class="modal">
         <div class="modal-content">
-        <span class="close">&times;</span>
+            <span class="close">&times;</span>
             <p style="text-align: center;">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
-            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-            </svg>
-            Order placed successfully.</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                </svg>
+                Order placed successfully.
+            </p>
         </div>
     </div>
 
