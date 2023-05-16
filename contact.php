7bb697f7f48cf0d6cc1facf144de96250de0f76c
UserXPC22 <cabasanxp@gmail.com>
2023-05-17 01:23:38 +0800

    updated the contact page

--- contact.php
+++ contact.php
@@ -56,6 +56,13 @@
     p {
       font-size: 15px;
     }
+
+    body{
+        background-color: rgba(238, 226, 203, 1);
+        background: linear-gradient(90deg, rgba(238, 226, 203, 1) 20%, rgba(173, 142, 121, 1) 100%);
+    }
+
+
   </style>
 
 </head>
@@ -186,9 +193,10 @@
           <p>Hi there, We are available 24/7 by fax, e-mail or by phone. Drop us line so we can talk further about that.<br><br>
             For more info or inquiry about our products project, and pricing. Feel free to get in touch with us.</p><br>
           <p><img src="images/location.png" width="30" height="30">&nbsp;&nbsp;Mandaue City, Cebu, Philippines</p>
-          <p><img src="images/email.png" width="30" height="30">&nbsp;&nbsp;<u>michaeljohn.bacalso2@gmail.com</u></p>
-          <p><img src="images/bluephone.png" width="30" height="30">&nbsp;&nbsp;<u>0977 022 8436</u></p><br>
-          <img src="images/handles.png" width="100" height="30"><br><br>
+          <p><img src="images/gmail.png" width="30" height="30">&nbsp;&nbsp;<u>michaeljohn.bacalso2@gmail.com</u></p>
+          <p><img src="images/redphone.png" width="30" height="30">&nbsp;&nbsp;<u>0977 022 8436</u></p><br>
+          <img src="images/facebook.png" width="30" height="30">&nbsp;&nbsp;<img src="images/twitter.png" width="30" height="30">&nbsp;&nbsp;
+          <img src="images/instagram.png" width="30" height="30">&nbsp;&nbsp;<img src="images/linkedin.png" width="30" height="30"><br><br>
           <p><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d981.762344989377!2d123.7208826!3d10.1766418!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a979445dd1d3c5%3A0xcb922f4c684dd0e5!2sBREGGETTE%20CARENDERIA!5e0!3m2!1sen!2sph!4v1683829613166!5m2!1sen!2sph" width="550" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </p>
 
