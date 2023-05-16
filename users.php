ff0f4ca2ecb25a3cac18a5fe7f9e488cc30c91f1
jlmarbs <130033706+jlmarbs@users.noreply.github.com>
2023-05-16 22:04:07 +0800

    renaming users title

--- users.php
+++ users.php
@@ -24,7 +24,7 @@ $result = $conn->query($sql);
 
 <head>
   <meta charset="UTF-8" />
-  <title>Homepage</title>
+  <title>Users</title>
   <link rel="stylesheet" href="css\style(code).css" />
   <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflaire.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
