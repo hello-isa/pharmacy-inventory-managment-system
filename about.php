7736fdc383f63d6f356ecc08c027cd0e25e2123e
jlmarbs <130033706+jlmarbs@users.noreply.github.com>
2023-05-16 16:54:57 +0800

    added code for about page (initial)

---
+++ about.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About Us | ALV Pharmacy</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+            font-family: 'Josefin Sans', sans-serif;
+            box-sizing: border-box;
+            overflow: hidden;
+        }
+        
+        .about{
+            width: 100%;
+            padding: 78px 0px;
+            background-color: rgba(238, 226, 203, 1);
+            background: linear-gradient(90deg, rgba(238, 226, 203, 1) 20%, rgba(173, 142, 121, 1) 100%);
+        }
+        
+        .about img{
+            height: auto;
+            width: 420px;
+        }
+        
+        .about-text{
+            width: 550px;
+        }
+        
+        .main {
+            width: 1130px;
+            max-width: 95%;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            margin: 0 auto;
+        }
+        
+        .about-text h1{
+            font-size: 80px;
+            text-transform: capitalize;
+            margin-bottom: 20px;
+        }
+        
+        .about-text h5{
+            font-size: 25px;
+            text-transform: capitalize;
+            margin-bottom: 25px;
+            letter-spacing: 2px;
+        }
+        
+        .about-text p{
+            letter-spacing: 1px;
+            line-height: 28px;
+            font-size: 18px;
+            margin-bottom: 45px;
+        }
+    </style>
+</head>
+
+<body>
+    <section class="about">
+        <div class="main">
+            <img src="logoo.png">
+            <div class="about-text">
+                <h1>About Us</h1>
+                <h5>ALV Pharmacy</h5>
+                <p>ALV Pharmacy is a trusted drugstore in Guadalupe, Cebu City, conveniently located across One Pavilion Mall. With over 20 years of experience, it has established itself as a professional medical service provider since November 8, 2001. In addition to its physical location, ALV Pharmacy also offers convenient services to assist customers both onsite and online. Customers visiting the store can expect knowledgeable staff who are ready to provide personalized assistance, answer questions, and offer expert advice on medications and healthcare products. ALV Pharmacy strives to meet the diverse needs of its customers, ensuring a positive and satisfactory experience for all.</p>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
