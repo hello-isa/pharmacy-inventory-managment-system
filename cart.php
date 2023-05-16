f63d48e302f94a368615d3e7bc2bdda7604711a6
jlmarbs <130033706+jlmarbs@users.noreply.github.com>
2023-05-16 22:00:29 +0800

    renaming cart title

--- cart.php
+++ cart.php
@@ -2,7 +2,7 @@
 
 <head>
   <meta charset="UTF-8" />
-  <title>Homepage</title>
+  <title>Cart | ALV Pharmacy</title>
   <link rel="stylesheet" href="css\style(code).css" />
   <link href="css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
